94f5df7633af719f519097d1b8281abe82d7f2ca
Alex <manakov55@yandex.ru>
2017-05-29 12:57:26 +0700

    quotes slider done

--- app/js/script.js
+++ app/js/script.js
@@ -10,3 +10,22 @@ $(function(){
   cssEase: 'linear'
 });
 });
+
+$(function(){
+  $('.quotes__slider').slick({
+  accessibility: true,
+  arrows: false,
+  dots: true,
+  infinite: true,
+  speed: 500,
+  fade: true,
+  cssEase: 'linear',
+  dotsClass: 'quotes__slider_dots',
+  // appendDots: $(.quotes)
+});
+});
+
+$("document").ready(function() {
+	$(".reasons__wrapper").find(".reason__heading").css("text-align", "center")
+	
+});
