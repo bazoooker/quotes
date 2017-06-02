c9aede0c09723cb83663819fa84673cf1f9f3e83
Alex <manakov55@yandex.ru>
2017-06-02 15:42:52 +0700

    new gulp file, new build model

--- dist/js/script.js
+++ dist/js/script.js
@@ -10,3 +10,26 @@ $(function(){
   cssEase: 'linear'
 });
 });
+
+$(function(){
+  $('.quotes__slider').slick({
+  accessibility: true,
+  arrows: false,
+  dots: true,
+  infinite: true,
+  speed: 500,
+  fade: true,
+  cssEase: 'linear',
+  dotsClass: 'quotes__slider_dots',
+  // appendDots: $(.quotes)
+});
+});
+
+$("document").ready(function() {
+	$(".nav__menu").on("click", function() {
+    $('.nav-main').toggleClass('nav-main_on');
+    $('.nav__menu').toggleClass('nav__menu_close');
+    $('.nav__menu_text').toggleClass('nav__menu_text_close');
+    // $('.nav__menu').empty();
+  });	
+});
--- gulpfile.js
+++ gulpfile.js
@@ -6,14 +6,14 @@ var gulp      	= require('gulp'), // Подключаем Gulp
 gulp.task('sass', function(){ // Создаем таск Sass
     return gulp.src('app/scss/**/*.scss') // Берем источник
         .pipe(sass()) // Преобразуем Sass в CSS посредством gulp-sass
-        .pipe(gulp.dest('app/css')) // Выгружаем результата в папку app/css
+        .pipe(gulp.dest('dist/css')) // Выгружаем результата в папку app/css
         .pipe(browserSync.reload({stream: true})) // Обновляем CSS на странице при изменении
 });
 
 gulp.task('browser-sync', function() { // Создаем таск browser-sync
     browserSync({ // Выполняем browserSync
         server: { // Определяем параметры сервера
-            baseDir: 'app' // Директория для сервера - app
+            baseDir: 'dist' // Директория для сервера - app
         },
         notify: true // Отключаем уведомления
     });
@@ -21,25 +21,8 @@ gulp.task('browser-sync', function() { // Создаем таск browser-sync
 
 gulp.task('watch', ['browser-sync', 'sass'], function() {
     gulp.watch('app/scss/**/*.scss', ['sass']); // Наблюдение за sass файлами
-    gulp.watch('app/*.html', browserSync.reload); // Наблюдение за HTML файлами в корне проекта
+    gulp.watch('dist/*.html', browserSync.reload); // Наблюдение за HTML файлами в корне проекта
     // Наблюдение за другими типами файлов
 });
 
-gulp.task('build', ['sass'], function() {
-
-    var buildCss = gulp.src([ // Переносим библиотеки в продакшен
-        'app/css/**/*'
-        ])
-    .pipe(gulp.dest('dist/css'))
-
-    var buildFonts = gulp.src('app/fonts/**/*') // Переносим шрифты в продакшен
-    .pipe(gulp.dest('dist/fonts'))
-
-    var buildJs = gulp.src('app/js/**/*') // Переносим скрипты в продакшен
-    .pipe(gulp.dest('dist/js'))
-
-    var buildHtml = gulp.src('app/*.html') // Переносим HTML в продакшен
-    .pipe(gulp.dest('dist'));
-
-});
 
